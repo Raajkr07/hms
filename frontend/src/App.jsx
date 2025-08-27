1f599a9fdf8889699f6ef4f1ba6e73e1b8a28aaa
RAJ KUMAR <rk8210032@gmail.com>
2025-08-28 01:54:27 +0530

    added router in this

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/index.css'
+import '@mantine/core/styles.css'
+import { createTheme, MantineProvider } from '@mantine/core';
+import AppRoutes from './routes/AppRoutes';
+import { BrowserRouter } from 'react-router-dom';
+
+const theme = createTheme({
+  focusRing:"never",
+  fontFamily: "poppins, sans-serif",
+  headings: {fontFamily: "merriweather, serif"},
+  colors: {
+    primary: ['#f1fcfa', '#cff8ef', '#9ff0e1', '#67e1cf', '#32b9a9', '#1fad9f', '#168b82', '#166f69', '#165955', '#174a47', '#072c2b'],  
+    neutral: ['#f6f6f6', '#e7e7e7', '#d1d1d1', '#b0b0b0', '#888888', '#6d6d6d', '#5d5d5d', '#4f4f4f', '#454545', '#3d3d3d', '#000000',],
+  },
+  primaryColor: "primary",
+  primaryShade: 4,
+  defaultGradient: {
+    from: "primary.4",
+    to: "primary.8",
+    deg: 132
+  }
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 }
 
 export default App
