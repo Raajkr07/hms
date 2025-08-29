20fd574530a8a66ed1952b48e0ee6201109b9c48
RAJ KUMAR <rk8210032@gmail.com>
2025-08-29 12:41:05 +0530

    initialize required micro services files

---
+++ backend/donation-service/src/test/java/com/hopemeds/donation/DonationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopemeds.donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
