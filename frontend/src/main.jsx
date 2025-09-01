07b175d10a283b63bc52ec7100a69a160c8e3983
RAJ KUMAR <rk8210032@gmail.com>
2025-09-01 14:09:06 +0530

    remove default scroll style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual';
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
