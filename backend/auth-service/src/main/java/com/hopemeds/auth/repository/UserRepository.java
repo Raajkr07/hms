6bfee3a2f5f282dc4168b887636fcff91425e16b
RAJ KUMAR <rk8210032@gmail.com>
2025-09-05 02:20:41 +0530

    made Interface for User

---
+++ backend/auth-service/src/main/java/com/hopemeds/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hopemeds.auth.repository;
+import com.hopemeds.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
