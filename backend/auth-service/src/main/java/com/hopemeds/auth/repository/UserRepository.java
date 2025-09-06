0d5d13054015864bfe68ed127544c3d5f31fdaab
RAJ KUMAR <rk8210032@gmail.com>
2025-09-06 19:44:15 +0530

    added unnesassary lines

--- backend/auth-service/src/main/java/com/hopemeds/auth/repository/UserRepository.java
+++ backend/auth-service/src/main/java/com/hopemeds/auth/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.hopemeds.auth.repository;
+
 import com.hopemeds.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
