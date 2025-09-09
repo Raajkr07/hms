f857722be10a882775406a566b57aa91b9431f8a
RAJ KUMAR <rk8210032@gmail.com>
2025-09-09 23:05:02 +0530

    made NGOs, Medical shop, Hospitals, Clinics showing page for user

---
+++ frontend/src/account/users/data/LocationData.jsx
@@ -0,0 +1,51 @@
+export const medicalStores = [
+  { id: 1, name: 'Apollo Pharmacy Naka No.5 Darbhanga', address: 'No. 148, Marriage Hall Rd, Rahamganj, Darbhanga, Bihar 846004, India', phone: '+91 79 4296 0815', rating: 4.3, hoursToday: '7 am–11 pm', url: 'https://www.apollopharmacy.in', categories: ['Pharmacy'] },
+  { id: 2, name: 'Chemist Box - Darbhanga', address: 'Om Balaji Complex, Tower Chowk, Bada Bazar, Lal Bagh, Darbhanga, Bihar 846004, India', phone: '+91 80 3722 1192', rating: 5.0, hoursToday: '9 am–9 pm', url: 'http://chemistbox.in', categories: ['Pharmacy'] },
+  { id: 3, name: 'B N H Drug Store', address: '970 B, Rajpura Rd, Ludhiana, Bihar 141001, India', phone: '+91 81464 21133', rating: 4.7, hoursToday: 'Open 24 hours', url: 'http://www.bassinursinghome.com', categories: ['Pharmacy'] },
+  { id: 4, name: 'Lucky Medicos', address: 'Booth no.9, Sarabha Nagar, Darbhanga, Bihar 846001', phone: '+91 62839 23104', rating: 4.7, hoursToday: '8:30 am–10 pm', url: 'http://luckymedicos.com', categories: ['Pharmacy', 'Medical Center'] },
+  { id: 5, name: 'Om Shanti Medicals', address: 'Rajendra Nagar, Darbhanga, Bihar', phone: '+91 98765 12345', rating: 4.1, hoursToday: '8 am–8 pm', url: '', categories: ['Pharmacy'] },
+  { id: 6, name: 'Harshita Medical Store', address: 'New Bazar, Darbhanga, Bihar', phone: '+91 98712 34567', rating: 4.6, hoursToday: '9 am–9 pm', url: '', categories: ['Pharmacy'] },
+  { id: 7, name: 'Vijay Medicals', address: 'Station Road, Darbhanga, Bihar', phone: '+91 98123 45678', rating: 4.5, hoursToday: '7 am–11 pm', url: '', categories: ['Pharmacy'] },
+  { id: 8, name: 'Shree Krishna Medicos', address: 'Civil Lines, Darbhanga, Bihar', phone: '+91 98798 76543', rating: 4.3, hoursToday: '8 am–10 pm', url: '', categories: ['Pharmacy'] },
+  { id: 9, name: 'City Medical Store', address: 'Laheriasarai, Darbhanga, Bihar', phone: '+91 98345 12345', rating: 4.4, hoursToday: '9 am–9 pm', url: '', categories: ['Pharmacy'] },
+  { id: 10, name: 'Sankalp Medicos', address: 'MG Road, Darbhanga, Bihar', phone: '+91 97654 32109', rating: 4.2, hoursToday: '8 am–8 pm', url: '', categories: ['Pharmacy'] },
+];
+
+export const ngos = [
+  { id: 1, name: 'Roti Bank Darbhanga NGO', address: 'Mirzapur, Lal Bagh, Darbhanga, Bihar 846004, India', phone: '', rating: 5.0, hoursToday: 'Contact for timing', url: 'https://rotibank.ngo', categories: ['Non-profit organization'] },
+  { id: 2, name: 'GREEN INDIA YOUTH WOMEN SOCIETY', address: 'Sundarpur, Gehumi, Darbhanga, Bihar 846005, India', phone: '+91 94728 78877', rating: 5.0, hoursToday: '8 am–5 pm', url: 'https://greenindiayouth.org', categories: ['Non-profit organization'] },
+  { id: 3, name: 'Mahila Jagriti Sansthan', address: 'Chhapki Paddi, Darbhanga, Bihar', phone: '+91 94314 98549', rating: 4.4, hoursToday: '10 am–6 pm', url: 'https://mahilajagriti.org', categories: ['Non-governmental organization'] },
+  { id: 4, name: 'Aahar Mahila Sanstha', address: 'Bhathiarisarai, Darbhanga, Bihar', phone: '+91 99737 32829', rating: 5.0, hoursToday: 'Open 24 hours', url: 'https://aaharmahila.org', categories: ['Non-governmental organization'] },
+  { id: 5, name: 'Child Welfare Society Darbhanga', address: 'Laheriasarai, Darbhanga, Bihar', phone: '', rating: 4.6, hoursToday: '9 am–5 pm', url: '', categories: ['Non-profit organization'] },
+  { id: 6, name: 'Darbhanga Volunteers', address: 'Darbhanga, Bihar', phone: '+91 70000 00000', rating: 4.8, hoursToday: 'Contact for timing', url: '', categories: ['Volunteering'] },
+  { id: 7, name: 'Sunrise Charitable Trust', address: 'Darbhanga, Bihar', phone: '', rating: 4.5, hoursToday: 'Open 9 am–6 pm', url: '', categories: ['Non-profit organization'] },
+  { id: 8, name: 'Helping Hands NGO', address: 'Darbhanga, Bihar', phone: '+91 90000 00000', rating: 4.7, hoursToday: '9 am–7 pm', url: '', categories: ['Non-profit organization'] },
+  { id: 9, name: 'Social Welfare Society Darbhanga', address: 'Laheriasarai, Darbhanga', phone: '', rating: 4.3, hoursToday: '10 am–5 pm', url: '', categories: ['Social_services'] },
+  { id: 10, name: 'Hope Foundation Bihar', address: 'Darbhanga, Bihar', phone: '+91 98000 00000', rating: 4.9, hoursToday: 'Open 24 hrs', url: '', categories: ['Charity'] },
+];
+
+export const hospitals = [
+  { id: 1, name: 'Darbhanga Medical College Hospital', address: 'DMCH Rd, Laheriasarai, Darbhanga, Bihar 846001, India', phone: '+91 6272 233 228', rating: 3.8, hoursToday: 'Open 24 hours', url: 'https://dmch.edu', categories: ['Government hospital', 'Pharmacy'] },
+  { id: 2, name: 'Global Clinicity Hospital', address: 'Banker’s Colony, Rahamganj, Darbhanga, Bihar 846004, India', phone: '+91 96354 60000', rating: 4.4, hoursToday: 'Open 24 hours', url: 'https://globalclinicity.com', categories: ['Hospital'] },
+  { id: 3, name: 'Paras Global Hospitals', address: 'VIP Rd, Laheriasarai, Darbhanga, Bihar 846003, India', phone: '+91 78350 84933', rating: 3.5, hoursToday: 'Open 24 hours', url: 'https://parashospitals.com/darbhanga/', categories: ['Hospital'] },
+  { id: 4, name: 'SN Sarraf Hospital', address: 'DMCH Rd, Mogalpura, Laheriasarai, Darbhanga, Bihar 846001, India', phone: '+91 72095 67555', rating: 4.9, hoursToday: 'Open 24 hours', url: 'https://snsarrafhospital.com', categories: ['Hospital'] },
+  { id: 5, name: 'Jogendra Memorial Medical Hospital', address: 'Hospital Road, Benta, Laheriasarai, Darbhanga, Bihar 846001, India', phone: '+91 6272 222 244', rating: 4.2, hoursToday: 'Open 24 hours', url: 'https://jmmh.in', categories: ['Hospital'] },
+  { id: 6, name: 'CARE KILKARI HOSPITAL & IVF CENTRE', address: 'VIP Rd, Laheriasarai, Darbhanga, Bihar 846003, India', phone: '+91 81780 00477', rating: 4.8, hoursToday: 'Open 24 hours', url: 'https://carekilkari.com', categories: ['Hospital', 'Maternity hospital'] },
+  { id: 7, name: 'Swarn Rekha Hospital', address: 'Civil Lines, Darbhanga, Bihar', phone: '+91 98765 43210', rating: 4.3, hoursToday: '9 am–9 pm', url: '', categories: ['Hospital'] },
+  { id: 8, name: 'Sai Hospital', address: 'Rajnagar, Darbhanga, Bihar', phone: '+91 98456 78901', rating: 4.1, hoursToday: '8 am–8 pm', url: '', categories: ['Hospital'] },
+  { id: 9, name: 'Life Care Hospital', address: 'Station Road, Darbhanga, Bihar', phone: '+91 97654 32109', rating: 4.4, hoursToday: '9 am–9 pm', url: '', categories: ['Hospital'] },
+  { id: 10, name: 'Maa Shakti Hospital', address: 'New Bazar, Darbhanga, Bihar', phone: '+91 98012 34567', rating: 4.2, hoursToday: '8 am–8 pm', url: '', categories: ['Hospital'] },
+];
+
+export const clinics = [
+  { id: 1, name: 'Lily Health Care Darbhanga', address: 'DMCH Rd, Laheriasarai, Darbhanga, Bihar 846001, India', phone: '+91 97083 31002', rating: 4.9, hoursToday: '9 am–3 pm', url: 'https://lilyhealthcare.com', categories: ['Medical clinic'] },
+  { id: 2, name: 'Sexologist Doctor Darbhanga', address: 'Sundarpur, Darbhanga, Bihar 846004, India', phone: '+91 94316 53151', rating: 5.0, hoursToday: '10 am–7 pm', url: 'https://healthsafeclinic.com', categories: ['Sexologist'] },
+  { id: 3, name: 'Dr Bhola Nayak Clinic', address: 'VIP Rd, Darbhanga, Bihar', phone: '+91 98354 58559', rating: 5.0, hoursToday: '8 am–7:30 pm', url: '', categories: ['Medical clinic'] },
+  { id: 4, name: 'Dr P S Jassal Clinic', address: 'Link Road, Darbhanga, Bihar 141005, India', phone: '+91 70548 47787', rating: 4.7, hoursToday: '10 am–1:30 pm', url: 'https://justdial.com', categories: ['Medical clinic'] },
+  { id: 5, name: 'Dr Sandeep Puri Clinic', address: 'Kitchlu Nagar, Darbhanga', phone: '+91 62801 48404', rating: 4.9, hoursToday: '8 am–9 pm', url: 'https://drpuriclinic.com', categories: ['Medical group'] },
+  { id: 6, name: 'Smile Dental Clinic', address: 'Rajendra Nagar, Darbhanga', phone: '+91 98765 67890', rating: 4.6, hoursToday: '9 am–5 pm', url: '', categories: ['Dental clinic'] },
+  { id: 7, name: 'City Skin Clinic', address: 'Civil Lines, Darbhanga', phone: '+91 98456 12345', rating: 4.3, hoursToday: '10 am–6 pm', url: '', categories: ['Skin care clinic'] },
+  { id: 8, name: 'Neuro Care Clinic', address: 'Station Road, Darbhanga', phone: '+91 97654 98765', rating: 4.4, hoursToday: '8 am–4 pm', url: '', categories: ['Neurology'] },
+  { id: 9, name: 'Apex Eye Clinic', address: 'New Bazar, Darbhanga', phone: '+91 98012 34567', rating: 4.2, hoursToday: '9 am–6 pm', url: '', categories: ['Ophthalmology'] },
+  { id: 10, name: 'Care Home Clinic', address: 'MG Road, Darbhanga', phone: '+91 98765 43210', rating: 4.5, hoursToday: '9 am–7 pm', url: '', categories: ['General Medicine'] },
+];
