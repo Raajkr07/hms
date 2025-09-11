677da2c4c26e7dd3703fa7a6f8e574d1dd60f03e
RAJ KUMAR <rk8210032@gmail.com>
2025-09-11 23:01:13 +0530

    made REST API method and it's endpoint

--- backend/payment-service/src/main/java/com/hopemeds/payment/controller/PaymentController.java
+++ backend/payment-service/src/main/java/com/hopemeds/payment/controller/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/api/payments")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class PaymentController {
