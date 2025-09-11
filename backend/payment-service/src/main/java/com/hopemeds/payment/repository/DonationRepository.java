8ed7e53ccc06902df59dbd1b4101ed8628bb4072
RAJ KUMAR <rk8210032@gmail.com>
2025-09-11 23:04:08 +0530

    Donation repo for storing the donation data

--- backend/payment-service/src/main/java/com/hopemeds/payment/repository/DonationRepository.java
+++ backend/payment-service/src/main/java/com/hopemeds/payment/repository/DonationRepository.java
@@ -3,8 +3,9 @@ package com.hopemeds.payment.repository;
 import com.hopemeds.payment.entity.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+    Optional<Donation> findByRazorpayOrderId(String razorpayOrderId);
 }
-
