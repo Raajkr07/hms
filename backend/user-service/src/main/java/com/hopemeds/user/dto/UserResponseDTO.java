464dfc5c6ba25ad5607af36c27b87059b41e445a
RAJ KUMAR <rk8210032@gmail.com>
2025-09-15 19:46:54 +0530

    made user response class

---
+++ backend/user-service/src/main/java/com/hopemeds/user/dto/UserResponseDTO.java
@@ -0,0 +1,13 @@
+package com.hopemeds.user.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponseDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+}
